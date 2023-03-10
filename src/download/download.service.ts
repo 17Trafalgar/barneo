def613e60bceacd254838f0a07c6d76cb835cc14
Arten <rybckin.rae2013@yandex.ru>
2023-03-10 11:06:17 +0700

    fix service

--- src/download/download.service.ts
+++ src/download/download.service.ts
@@ -4,11 +4,13 @@ import { UrlDto } from './Dto/url.dto';
 import * as xlsx from 'xlsx';
 import * as fs from 'fs/promises';
 
+const path = './uploadedFiles/target.xlsx';
+
 @Injectable()
 export class DownloadService {
   constructor(private readonly axios: HttpService) {}
 
-  public async getFileByUrl({ url }): Promise<string> {
+  public async getFileByUrl({ url }): Promise<any> {
     try {
       const response = await this.axios.axiosRef.get(url, {
         responseType: 'stream',
@@ -19,12 +21,8 @@ export class DownloadService {
           .on('data', (chunk) => {
             buffer.push(chunk);
           })
-          //TODO: скачать файл и отдать путь до него
-          .on('end', () => {
-            // если нет папки то создать её
-            Buffer.concat(buffer) //сохранить файл
-            resolve(`PATH`) //вернуть путь на файл
-          })
+          .on('event', () => fs.writeFile(path, url))
+          .on('end', () => resolve(Buffer.concat(buffer)))
           .on('error', reject);
       });
     } catch (error) {
@@ -35,30 +33,20 @@ export class DownloadService {
   public async xlsxToJson(urlDto: UrlDto): Promise<any> {
     try {
       const buffer = await this.getFileByUrl(urlDto);
-      const data = xlsx.read(buffer, { type: 'buffer' }); // читать из папки
+      console.log({ buffer });
+      const data = xlsx.read(buffer, { type: 'buffer' });
       const finalObject = {};
       data.SheetNames.forEach((sheetName) => {
-        const listObject = xlsx.utils.sheet_to_json(data.Sheets[sheetName]);
-        finalObject[sheetName] = listObject;
+        const rowObject = xlsx.utils.sheet_to_json(data.Sheets[sheetName]);
+        finalObject[sheetName] = rowObject;
         console.log(finalObject);
       });
-      await fs.writeFile(
-        './uploadedFiles/price-list.json',
-        JSON.stringify(finalObject),
-      );
-      return 'file upload'; // не сохранять в файл а возвращать из функции json
+      /* await fs.writeFile(path, JSON.stringify(finalObject)); */
     } catch (error) {
       throw new Error(error);
     }
   }
-
-  //TODO:создать функцию
-  /**
-   * временная функция для тестирования получает ссылку на файл из контроллера запускает функции
-   * getFileByUrl и xlsxToJson и возвращает результат контроллеру
-   * @param path
-   */
-  async main(urlDto: UrlDto): Promise<string>{
+  async main(urlDto: UrlDto): Promise<string> {
     const path = await this.getFileByUrl(urlDto);
     const json = await this.xlsxToJson(path);
     return json;
