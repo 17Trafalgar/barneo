b05ac856a496da94f71273e6f9db057b1c213c95
Arten <rybckin.rae2013@yandex.ru>
2023-05-03 19:00:22 +0700

    fix clien.service

--- src/download/client/client.service.ts
+++ src/download/client/client.service.ts
@@ -4,27 +4,53 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class ClientService {
   constructor(private readonly Axios: HttpService) {}
+  username = 'info@barneo.ru';
+  password = '12345678';
 
   authBasic = {
     Authorization:
       'Basic ' +
-      Buffer.from('info@barneo.ru' + ':' + '12345678').toString('base64'),
+      Buffer.from(this.username + ':' + this.password).toString('base64'),
   };
   domen = 'https://api.pbd.complexbar.ru';
 
+  async getUrl(uri: string): Promise<string> {
+    try {
+      const url = decodeURI(this.domen + uri);
+      return url;
+    } catch (error) {
+      console.log(error);
+      throw new Error(error);
+    }
+  }
+
   async getStock(): Promise<any> {
-    const url = decodeURI(this.domen + '/stocks');
-    const response = await this.Axios.axiosRef.get(url, {
-      headers: this.authBasic,
-    });
-    return response.data;
+    try {
+      const response = await this.Axios.axiosRef.get(
+        await this.getUrl('/stocks'),
+        {
+          headers: this.authBasic,
+        },
+      );
+      return response.data;
+    } catch (error) {
+      console.log(error);
+      throw new Error(error);
+    }
   }
 
   async getPrice(): Promise<any> {
-    const url = decodeURI(this.domen + '/prices');
-    const response = await this.Axios.axiosRef.get(url, {
-      headers: this.authBasic,
-    });
-    return response.data;
+    try {
+      const response = await this.Axios.axiosRef.get(
+        await this.getUrl('/prices'),
+        {
+          headers: this.authBasic,
+        },
+      );
+      return response.data;
+    } catch (error) {
+      console.log(error);
+      throw new Error(error);
+    }
   }
 }
